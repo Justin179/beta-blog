4de8764315bd29ed5d578c108ebe35b16b2193d0
Justin Chen <justin.uob@gmail.com>
2016-05-29 03:07:59 +0000

    Add particles and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
-  
+
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
@@ -39,7 +39,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
 
   private
   # white list (開洞之意) the incoming params
